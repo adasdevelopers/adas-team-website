ac640eb1b4fc2c845201cd413345490b0901c4c0
orackle <42990726+orackle@users.noreply.github.com>
2020-11-12 14:22:16 -0700

    feat: implemented header (#27)
    * fixed html file (#1)
    
    * saving
    
    * added index.html
    
    Co-authored-by: Nana Andoh <nanaegyir2000@hotmail.com>
    
    * Update .gitignore
    
    * Update .gitignore
    
    * feat: created initial files for the different pages and started on the footer using the html generator plugin.
    
    * feat: separated Address and Navigation sections in footer
    
    * fix: merged with master to add index.html (#18) (#3)
    
    * feat: added index.html (#15)
    
    * saving
    
    * added index.html
    
    * feat: started with FAQ page
    
    * fix: fixed gitignore
    
    * fix: changed gitignore again
    
    * fix: deleted _site directory (#16)
    
    * saving
    
    * added index.html
    
    * deleted _site dir
    
    * fix: merge issue with staging
    
    * fix: merged with master to add index.html (#18)
    
    * saving
    
    * added index.html
    
    * deleted _site dir
    
    * refactor: renamed html files and tested new directory structure
    
    Co-authored-by: Nana Andoh <59236774+deadpools-besty@users.noreply.github.com>
    Co-authored-by: Della <humanita.1999@gmail.com>
    
    * refactor: updated layouts (#5)
    
    * feat: created initial files for the different pages and started on the footer using the html generator plugin.
    
    * feat: separated Address and Navigation sections in footer
    
    Co-authored-by: Della <humanita.1999@gmail.com>
    
    * refactor: created sass folders
    
    * fix: updated README.md with labelled links
    
    * feat:header initial
    
    * Update index.html
    
    * refactor:implemented header
    [] need to implement mobile version fully
    [] need to finish tablet
    
    * merge conflicts
    
    Co-authored-by: Nana Andoh <nanaegyir2000@hotmail.com>
    Co-authored-by: Della <humanita.1999@gmail.com>
    Co-authored-by: Nana Andoh <59236774+deadpools-besty@users.noreply.github.com>

---
+++ assets/js/navbar.js
@@ -0,0 +1,18 @@
+/* Open the sidenav */
+function openNav() {
+
+  // Look for .hamburger
+  var hamburger = document.querySelector(".hamburger");
+  // On click
+  hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
