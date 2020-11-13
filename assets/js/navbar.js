e73eaeea1cd9e66851a80abcbd810b1df592dce2
orackle <42990726+orackle@users.noreply.github.com>
2020-11-13 11:09:11 -0700

    Front page (#9)
    * feat: front page rough elements
    
    missing footer, commented out
    
    * refactor: front page footer css
    
    * more css factoring

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,18 +1,6 @@
-/* Open the sidenav */
-function openNav() {
-
-  // Look for .hamburger
-  var hamburger = document.querySelector(".hamburger");
-  // On click
-  hamburger.addEventListener("click", function() {
-    // Toggle class "is-active"
-    hamburger.classList.toggle("is-active");
-    // Do something else, like open/close menu
-
-  document.getElementById("mySidenav").style.width = "100%";
-}
-
-/* Close/hide the sidenav */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+const sidebar = document.querySelector('.sidebar');
+const mainContent = document.querySelector('.main-content');
+document.querySelector('button').onclick = function () {
+  sidebar.classList.toggle('sidebar_small');
+  mainContent.classList.toggle('main-content_large')
 }
