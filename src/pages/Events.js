b209aec85d829b4d15c958c1e996fde669fd5bb6
Nadeen <nadeen@ualberta.ca>
2021-01-02 17:35:59 -0700

    refactor: move from vanilla html/js/css to react

---
+++ src/pages/Events.js
@@ -0,0 +1,9 @@
+function Events() {
+  return (
+    <div>
+      <h1>Events</h1>
+    </div>
+  );
+}
+
+export default Events;
---
+++ src/pages/JobBoard.js
@@ -0,0 +1,9 @@
+function JobBoard() {
+  return (
+    <div>
+      <h1>Job Board</h1>
+    </div>
+  );
+}
+
+export default JobBoard;
