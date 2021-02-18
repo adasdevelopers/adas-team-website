93f4e9cbeafa888f38687cc9b9a9562d9215a675
Nadeen Mohamed <nadeen@ualberta.ca>
2021-02-18 14:17:50 -0700

    ✨feat: initialize firebase (#50)

--- src/pages/JobBoard.js
+++ src/pages/JobBoard.js
@@ -1,3 +1,17 @@
+import { db } from "../firebase";
+
+db.collection("job-postings")
+  .get()
+  .then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+  })
+  .catch((error) => {
+    console.log("Error getting documents: ", error);
+  });
+
 function JobBoard() {
   return (
     <div>
