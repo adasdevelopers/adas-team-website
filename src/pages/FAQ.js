faf8ceb2fabce9ad612ddc30afe2f77270948a29
Nadeen <nadeen@ualberta.ca>
2021-02-18 19:01:29 -0700

    ✨ refactor: remove hardcoded components from FAQ

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -10,31 +10,23 @@ function FAQ() {
       </div>
 
       <div id="questions">
-        <div id="q1" class="left-question">
-          <h2>{questions[0]["question"]}</h2>
-          <h3>{questions[0]["answer"]}</h3>
-        </div>
-        <div id="q2" class="right-question">
-          <h2>{questions[1]["question"]}</h2>
-          <h3>{questions[1]["answer"]}</h3>
-        </div>
-        <div id="q3" class="left-question">
-          <h2>{questions[2]["question"]}</h2>
-          <h3>{questions[2]["answer"]}</h3>
-        </div>
-        <div id="q4" class="right-question">
-          <h2>{questions[3]["question"]}</h2>
-          <h3>{questions[3]["answer"]}</h3>
-        </div>
-        <div id="q5" class="left-question">
-          <h2>{questions[4]["question"]}</h2>
-          <h3>{questions[4]["answer"]}</h3>
-        </div>
-        <div id="q6" class="right-question">
-          <h2>{questions[5]["question"]}</h2>
-          <h3>{questions[5]["answer"]}</h3>
-        </div>
-        <div id="questions"></div>
+        {questions.map((question, i) => {
+          if (i % 2 == 0) {
+            return (
+              <div id={`q${i}`} className="left-question">
+                <h2>{question["question"]}</h2>
+                <h3>{question["answer"]}</h3>
+              </div>
+            );
+          } else {
+            return (
+              <div id={`q${i}`} className="right-question">
+                <h2>{question["question"]}</h2>
+                <h3>{question["answer"]}</h3>
+              </div>
+            );
+          }
+        })}
       </div>
     </div>
   );
