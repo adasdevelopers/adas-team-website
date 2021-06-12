d67b2a426a2dd661253c7f7eca05616e9a9b749e
Nana Andoh <59236774+deadpools-besty@users.noreply.github.com>
2021-06-11 22:53:33 -0600

    Contains fixes for job board and navbar (#94)
    * About page (#31)
    
    * refactor: changed from css to sass and created sass files
    
    * fix: created hyperlink for faq from _navigation.html
    
    * feat: set up hyperlinks and added titles to pages
    
    * feat: worked on footer a bit more. elements still need to be marginalized properly
    
    * feat: added text to about page
    
    * feat: completed json file for executives
    
    * feat: added .json values into About page
    
    * feat: front page basic elements in progress (#33)
    
    * feat: fix header css to sass
    
    * feat: front page rough elements
    
    missing footer, commented out
    
    * feat: front page rough elements (#8)
    
    missing footer, commented out
    
    * refactor: front page footer css
    
    * more css factoring
    
    * fix: readded footer
    
    * feat: completed majority of faq page content and styling
    
    * fix: fixed issue with css not showing up in deployment
    
    * fix: finalizing merge conflicts
    
    * edited config.yml
    
    * initialized firebase.js
    
    * updated .gitignore
    
    * saving
    
    * feat: got FAQ page to pull from firestore to display questions
    
    * chore: fixed routes in App.js:
    
    * commiting before merge with upstream
    
    * continued working on firebase implementation on main pages
    
    * added missing image files
    
    * added available jobs on job board page
    
    * fixed left and right alignment on FAQ page and added counter on job board
    
    * fixed conflicts with upstream
    
    * added homepage and deployment variables for deploying on github pages
    
    * made pink dividers on FAQ page responsive
    
    * saving for update
    
    * fixed some stuff
    
    * added blank user photo
    
    Co-authored-by: Della <humanita.1999@gmail.com>
    Co-authored-by: orackle <42990726+orackle@users.noreply.github.com>

--- src/pages/JobBoard.js
+++ src/pages/JobBoard.js
@@ -3,6 +3,7 @@ import React from 'react';
 import db from "../firebase";
 import { render } from '@testing-library/react';
 import Header from '../components/Header'
+import { Button } from 'react-scroll';
 
 
 /**
@@ -39,8 +40,8 @@ class JobBoard extends React.Component {
                 className="page"
             >
                 <Header title="Job Board" subtitle=" Search through our current job listings from various companies to get in touch and land your next job."></Header>
-
-
+             
+                <            div className="md:mx-48 sm:mx-20 lg:mx-48 xl:mx-72">
                 <h2>
                     AVAILABLE JOBS
                 </h2>
@@ -53,23 +54,16 @@ class JobBoard extends React.Component {
                         this.state.jobs && this.state.jobs.length
                     } job listings
                 </div>
-
+  
                 <div
                     id="job-posting"
                     className="blue-rect rounded-xl">
                     {
                         this.state.jobs &&
-                        this.state.jobs.map( job => {
-                            return(
-                            <div className="mx-4">
-                                <p className="font-title">
-                                    <b>{ job.job_title }</b> <br />
-                                    { job.company } <br />
-                                    { job.website }
-                                </p>
-                            </div>
+                        this.state.jobs.map( ({job_title, company, description, website, apply_link, email, image}, i) => (
+                                <JobPosting key={`jobposting${i}`} title={job_title} company={company} description={description} website={website} apply_link={apply_link} email={email} image={image}/>
                             )
-                        })
+                        )
                     }
                 </div>
 
@@ -87,11 +81,30 @@ class JobBoard extends React.Component {
                         POST A JOB
                     </button>
                 </div>
+                </div>
             </div>
 
         )
     }
 
 }
+
+const JobPosting = ({title, company, website, description, apply_link, email, image}) => (
+    <div className="mx-4 flex">
+        <img className="rounded-full h-24 w-24  mr-16" src={image ? image : ""} />
+        <div>
+            <div className="font-title">
+                <h3>{ title }</h3>
+                <p>{ company }</p>
+                <p> {description}</p>
+                <a href={ website }>Website</a>
+            </div>
+            <div className="mt-4 flex justify-between">
+                {email && <a  id="email" className="flex-1 mr-2 border-2 border-solid border-blue bg-white px-2 py-1 text-blue font-semibold rounded uppercase" href={`mailto:${email}`}>Contact</a>}
+                {apply_link && <a id="application_link" className="flex-1 mr-2 border-2 border-blue bg-blue px-2 py-1 text-white font-semibold rounded uppercase" href={apply_link} >Apply</a>}
+            </div>
+        </div>
+    </div>
+)
 
 export default JobBoard;
