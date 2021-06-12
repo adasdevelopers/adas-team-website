0e666ed25b602c190792fe2cca9d86e755f69772
orackle <debangan@ualberta.ca>
2021-06-12 02:12:31 -0600

    mostly done

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ import Typist from 'react-typist';
 function Header({title, subtitle, additional, homepage}) {
     return (
       <header>
-          <h1 className={homepage?"font-title text-7xl md:text-blue animate-fade-in-down": "text-left mx-48"}>{title}</h1>
+          <h1 className={homepage?"font-title text-7xl md:text-blue animate-fade-in-down": "text-left lg:mx-48 mx-8"}>{title}</h1>
           <p
-              className="font-body italic text-lg mx-48">
+              className="font-body italic text-lg lg:mx-48 mx-8">
               <Typist>
               <Typist.Delay ms={1800}/>
               {subtitle}
