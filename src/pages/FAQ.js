2e71c0296ed6f73e7195e5e580495962e2da0b17
Della <humanita.1999@gmail.com>
2021-06-20 16:55:19 -0600

    chore: added ARIA accessibility to FAQ, Join, Connect (#104)
    * refactor: changed from css to sass and created sass files
    
    * fix: created hyperlink for faq from _navigation.html
    
    * feat: set up hyperlinks and added titles to pages
    
    * feat: worked on footer a bit more. elements still need to be marginalized properly
    
    * feat: added text to about page
    
    * About page (#31)
    
    * refactor: changed from css to sass and created sass files
    
    * fix: created hyperlink for faq from _navigation.html
    
    * feat: set up hyperlinks and added titles to pages
    
    * feat: worked on footer a bit more. elements still need to be marginalized properly
    
    * feat: added text to about page
    
    * feat: completed json file for executives
    
    * feat: added .json values into About page
    
    * feat: front page basic elements in progress (#33)
    
    * feat: fix header css to sass
    
    * feat: front page rough elements
    
    missing footer, commented out
    
    * feat: front page rough elements (#8)
    
    missing footer, commented out
    
    * refactor: front page footer css
    
    * more css factoring
    
    * fix: readded footer
    
    * feat: completed majority of faq page content and styling
    
    * About page and updated README (#36)
    
    * refactor: changed from css to sass and created sass files
    
    * fix: created hyperlink for faq from _navigation.html
    
    * feat: set up hyperlinks and added titles to pages
    
    * feat: worked on footer a bit more. elements still need to be marginalized properly
    
    * feat: added text to about page
    
    * feat: completed json file for executives
    
    * feat: added .json values into About page
    
    * refactor: styled About page
    
    * feat: continued styling About page
    
    * fix: changed margins in executive cards
    
    * fix: updated README
    
    * fix: .gitignore change
    
    * chore: installed tailwind
    
    * chore: tried to get from firebase
    
    * feat: standardized footer style
    
    * refactor: home page with tailwind; fix: footer stuck to bottom of page
    
    * feat: added animation to hamburger menu
    
    * feat: added elements to nav
    
    * fix: added links hamburger menu
    
    * fix: added files to gitignore
    
    * fix: switched to windows terminal ubuntu from git bash
    
    * refactor: worked on about page
    
    * feat: styled About page and setup base styling across pages. issue: JobBoard page not displaying anything
    
    * Delete yarn.lock
    
    * Delete package-lock.json
    
    * fix: added yarn.lock to gitignore
    
    * fix: yarn.lock again
    
    * feat: styled pages with the base
    
    * fix: about page and set up ssh
    
    * fix: frickin yarn.lock again
    
    * modified readme
    
    * fix: div tag errors
    
    * Update README.md
    
    * Update README.md
    
    * feat: added links to Footer, made additional join links to About page
    
    * fix: fixed flexbox issues in About page; feat: added buttons
    
    * feat: added formspree into Connect page
    
    * fix: standardized base and component styles
    
    * feat: completed Connect page
    
    * feat: styled Join page, need to add links
    
    * fix: added spacing in join page, need to figure out what to do after user hits Subscribe
    
    * feat: added calendar iframe
    
    * chore: cleaned up Navigation to use props, removed redundant sass code
    
    * refactor: combined duplicated sections into a single component and deleted redundant sass files
    
    * fix: modified navigation to be more responsive; one anchor is acting weird
    
    * refactor: adjusted indents and added comments to About.js
    
    * refactor: added comments and modified header
    
    * refactor: added comments and TODO comments
    
    * chore: deleted sass
    
    * small edit
    
    * deleted experimental nav
    
    * feat: completed desktop elements in Join
    
    * feat: added form to Home
    
    * feat: added text to faq
    
    * merging upstream
    
    * chore: checked kb accessibility on forms
    
    * chore: added ARIA accessibility to FAQ, Join, Connect
    
    Co-authored-by: orackle <42990726+orackle@users.noreply.github.com>
    Co-authored-by: Nana Andoh <nanaegyir2000@hotmail.com>

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -46,69 +46,77 @@ class FAQ extends React.Component {
 				id="faq-page"
 				className="max-w-7xl lg:mx-auto page animate-fade-in-down font-body overflow-hidden"
 			>
-				<div
-					id="faq_image"
-					className="max-w-4xl mx-auto flex flex-col items-center justify-between lg:flex-row-reverse animate-fade-in-down"
-				>
-					{/* FAQ image */}
-					<img
-						id="faq"
-						src={faq}
-						alt="people sitting on infographics"
-						className="hidden md:inline-block w-screen px-16 lg:w-auto lg:px-0 lg:h-72"
-					/>
+                        <main>
+                              <div
+                                    id="faq_image"
+                                    className="max-w-4xl mx-auto flex flex-col items-center justify-between lg:flex-row-reverse animate-fade-in-down"
+                              >
+                                    <title>
+                                          {/* FAQ image */}
+                                          <img
+                                                id="faq"
+                                                src={faq}
+                                                alt="people sitting on infographics"
+                                                className="hidden md:inline-block w-screen px-16 lg:w-auto lg:px-0 lg:h-72"
+                                          />
+                                    
+                                          <Header 
+                                                title="FAQ" 
+                                                subtitle="Answering your most frequently asked questions." />
+                                    </title>
+                              </div>
 
-					<Header title="FAQ" subtitle="Answering your most frequently asked questions." />
-				</div>
+                              <section>
+                                    <div id="faq-section">
+                                          {this.state.questions &&
+                                                this.state.questions.map((question, i) => {
+                                                      return (
+                                                            <div
+                                                                  data-aos={question.left ? "fade-right" : "fade-left"}
+                                                                  data-aos-duration="500"
+                                                                  data-aos-easing="ease-in-sine"
+                                                            >
+                                                                  <div
+                                                                        className={`bg-light-blue w-11/10 lg:p-3 shadow-md rounded-3xl my-7 py-12 md:w-full lg:w-3/4 ${
+                                                                              i % 2 == 0
+                                                                                    ? "-ml-12 pl-16 pr-12 sm:-ml-18 md:-ml-20 md:pl-32 lg:ml-0"
+                                                                                    : "-mr-12 pr-16 pl-12 sm:-mr-18 md:-mr-20 md:pr-32 ml-auto lg:mr-0"
+                                                                        }`}
+                                                                  >
+                                                                        <div id="faq-question-and-answer" className="lg:px-14 lg:my-8">
+                                                                              <h3 id="faq-question" className="font-title text-xl mb-3 text-black font-bold">
+                                                                                    {question.question}
+                                                                              </h3>
+                                                                              <p id="faq-answer" className="font-light">
+                                                                                    {question.answer}
+                                                                              </p>
+                                                                              <div
+                                                                                    className={`divider-thin mt-8 w-6/5 lg:w-full lg:ml-0 lg:mr-0 ${
+                                                                                          i % 2 == 0 ? "-ml-20 sm:-ml-32 md:-ml-44" : "-mr-12"
+                                                                                    }`}
+                                                                              ></div>
+                                                                        </div>
+                                                                  </div>
+                                                            </div>
+                                                      );
+                                                })}
+                                    </div>
+                              </section>
 
-				<div id="faq-section">
-					{this.state.questions &&
-						this.state.questions.map((question, i) => {
-							return (
-								<div
-									data-aos={question.left ? "fade-right" : "fade-left"}
-									data-aos-duration="500"
-									data-aos-easing="ease-in-sine"
-								>
-									<div
-										className={`bg-light-blue w-11/10 lg:p-3 shadow-md rounded-3xl my-7 py-12 md:w-full lg:w-3/4 ${
-											i % 2 == 0
-												? "-ml-12 pl-16 pr-12 sm:-ml-18 md:-ml-20 md:pl-32 lg:ml-0"
-												: "-mr-12 pr-16 pl-12 sm:-mr-18 md:-mr-20 md:pr-32 ml-auto lg:mr-0"
-										}`}
-									>
-										<div id="faq-question-and-answer" className="lg:px-14 lg:my-8">
-											<h4 id="faq-question" className="font-title text-xl mb-3">
-												{question.question}
-											</h4>
-											<p id="faq-answer" className="font-light">
-												{question.answer}
-											</p>
-											<div
-												className={`divider-thin mt-8 w-6/5 lg:w-full lg:ml-0 lg:mr-0 ${
-													i % 2 == 0 ? "-ml-20 sm:-ml-32 md:-ml-44" : "-mr-12"
-												}`}
-											></div>
-										</div>
-									</div>
-								</div>
-							);
-						})}
-				</div>
+                              <div className="divider-thick mt-16 mb-4" />
 
-				<div className="divider-thick mt-16 mb-4" />
+                              <div>
+                                    <h2 className="pt-4 pb-7">Still have questions?</h2>
 
-				<div>
-					<h2 className="pt-4 pb-7">Still have questions?</h2>
-
-					<p>
-						Please feel free to reach out to us at:
-						<br />
-						<a href="mailto:adasteam@ualberta.ca" className="text-blue font-semibold">
-							adasteam@ualberta.ca
-						</a>
-					</p>
-				</div>
+                                    <p>
+                                          Please feel free to reach out to us at:
+                                          <br />
+                                          <a href="mailto:adasteam@ualberta.ca" className="text-blue font-semibold">
+                                                adasteam@ualberta.ca
+                                          </a>
+                                    </p>
+                              </div>
+                        </main>
 			</div>
 		);
 	}
